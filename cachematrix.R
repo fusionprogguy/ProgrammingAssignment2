43eaa79c9c20fc978d0c8e7660684784e2c59e5d
Steven Muschalik <steven.muschalik@gmail.com>
2015-05-24 21:22:08 +1000

    space

--- cachematrix.R
+++ cachematrix.R
@@ -16,23 +16,28 @@ makeCacheMatrix <- function(x = matrix()) {
                 #clear the matrix
                 m <- NULL
                 set <- function(y) {
+                        #save whatever is being saved in the set function to x
                         x <<- y
                         #reset the matrix to NULL
                         m <<- NULL
                 }
                 get <- function() {
+                        #return whatever is in the main function
                         x
                 }
         
                 setinverse  <- function(inverse) {
+                        #saves what is stored as the inverse into m
                         m <<- inverse
                 }
         
                 getinverse <- function() {
+                        #return what is in the m variable
                         m
                 }
                 
-                #declare the functions and return them as a list
+                #Declare the functions and return them as a list
+                #When makeCacheMatrix is called, any object can have these functions applied
                 list(set = set, get = get,
                      setinverse = setinverse,
                      getinverse = getinverse)
@@ -51,6 +56,7 @@ cacheSolve <- function(x, ...) {
         #then the cachesolve should retrieve the inverse from the cache.
         
         #retrieve the inverse if it exists
+        #subset the main function using "$" + main function 
         m <- x$getinverse()
         
         #The first thing cachemean does is to verify the value m, stored
@@ -58,19 +64,20 @@ cacheSolve <- function(x, ...) {
         #If it exists in memory, it simply returns a message and the
         #value m, that is supposed to be the mean, but not necessarily.
         
-        #if the matrix is empty in the current environment, get the cached inverse
+        #if the matrix is empty in the current environment (or not set to NULL), 
+        #get the cached inverse
         if(!is.null(m)) {
                 message("getting cached inversed data")
                 return(m)
         }
         
-        
+        #get the data stored in makeCacheMatrix
         data <- x$get()
         
         #calculate the inverse and save to m
         m <- solve(data, ...)
         
-        #save the cached inverse of m and return m
+        #save the cached inverse of m into the object assigned in makeCacheMatrix
         x$setinverse(m)
         m
 
